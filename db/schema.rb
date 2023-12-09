4356fb121cfd450e12c306f197d0bb21c76c6d0d
DESKTOP-G09MFLI\csyo1 <ncky24048@mineo.jp>
2023-12-10 04:21:03 +0900

    add user function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_08_080912) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_09_174717) do
   create_table "cart_details", force: :cascade do |t|
     t.integer "item_id", null: false
     t.integer "cart_id", null: false
@@ -57,9 +57,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_080912) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "cart_details", "carts"
