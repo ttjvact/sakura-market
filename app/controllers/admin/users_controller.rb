7048627e017f2011135eb334deee9445e072bf46
DESKTOP-G09MFLI\csyo1 <ncky24048@mineo.jp>
2023-12-13 05:09:24 +0900

    add edit user action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,25 @@
 class Admin::UsersController < ApplicationController
     before_action :auth_admin_user
     def index
-    
+        @users = User.all
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if user_permit[:password].blank?
+            user_permit.delete(:password)
+            user_permit.delete(:password_confirmation)
+          end
+        @user.update(user_permit)
+        redirect_to admin_users_path
+    end
+
+    private
+    def user_permit
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
